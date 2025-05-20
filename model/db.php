eed80f87ae0cae2a6a11eb5a106a390a3cc04f94
John Doe <mariamello.ads@gmail.com>
2025-05-20 15:52:10 -0300

    Add error handling attributes to PDO connection in db.php

--- model/db.php
+++ model/db.php
@@ -7,3 +7,7 @@ $db_user = "root";
 $db_pass = "";
 
 $conn = new PDO("mysql:dbname=$db_name;host=$db_host", $db_user, $db_pass);
+
+// aquii eu vou habilitar os erros
+$conn->setAttribute(PDO::   ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+$conn->setAttribute(PDO::ATTR_EMULATE_PREPARES, false); //isso faz com que EXPLODA O ERRO NA TELA PRA DEBUGARM MAIS RAPIDP

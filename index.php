86a70e2133ffb40fffd1032e0c593131e54bd36b
John Doe <mariamello.ads@gmail.com>
2025-05-20 15:17:02 -0300

    Add initial HTML structure and CSS for MOVIESTAR project

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>MOVIESTAR</title>
+    <link rel="short icon" href="assets/img/moviestar.ico">
+    <!-- CSS only -->
+    <link rel="stylesheet" href="view/css/style.css">
+
+</head>
+<body>
+   <h1>testando</h1> 
+</body>
+</html>
